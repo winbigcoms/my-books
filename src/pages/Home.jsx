bca5a301d98eb95192630ba5332c5bfe9c61969b
winbigcoms <bigcoms6290@gmail.com>
2020-08-02 03:14:46 +0900

    add sort function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,31 @@
 // @flow
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { withAuth } from '../HOCs/withAuth';
 import axios from 'axios';
 function Home (props) {
-    const [states,setState] = useState({
-    books:[{title:"ds"}],
+  
+  const [states,setState] = useState({
+    books:[],
     loading:false,
-    error:null
+    error:null,
+    sort:"title"
   });
+  const sortWhat = useRef(null)
+
+  function titleSort(key){
+    return (a,b)=> (a[key] > b[key] ? 1 : (a[key] < b[key] ? -1 : 0))
+  }
 
   useEffect( ()=>{
     async function initialize(){
       try{
-        const res = await axios.get("https://api.marktube.tv/v1/book",{headers:{
-          Authorization:`Bearer ${props.token}`
-        }});
+        const res = await axios.get("https://api.marktube.tv/v1/book",{
+          headers:{Authorization:`Bearer ${props.token}`}
+        });
         const books = res.data;
-        setState(...states,books);
+        books.sort(titleSort(states.sort));
+        console.log(books)
+        setState({states,books});
       }catch(e){
         console.log(e);
         setState({...states,error:e})
@@ -25,9 +34,32 @@ function Home (props) {
     initialize()
   },[])
 
-    return (
-      <div>
-      </div>
-    );
+  function catchSortWhat(e){
+    let copyBooks = states.books;
+    copyBooks.sort(titleSort(e.target.value));
+
+    setState({...states,books:copyBooks,sort:e.target.value});
+  }
+  return (
+    <div>
+      <ul>
+        {states.books.map(book =>(<li key={book.bookId}>
+          <h3>제목: {book.title}</h3>
+          <p>요약: {book.message}</p>
+          <p>저자: {book.author}</p>
+          <p>링크: {book.url}</p>
+          <p>책장에 추가한 날짜: {book.createdAt.slice(0,19)}</p>
+          <p>마지막 업데이트: {book.updatedAt.slice(0,10)}</p>
+        </li>))}
+      </ul>
+      <label>정렬
+        <select name="정렬" value={states.sort} ref={sortWhat} onChange={catchSortWhat}>
+          <option value="title">제목</option>
+          <option value="author">저자</option>
+          <option value="createdAt">추가날짜</option>
+        </select>
+      </label>
+    </div>
+  );
 };
 export default withAuth(Home, true);
