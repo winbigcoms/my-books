4720f277c25668e6e693050ddff77a0f7f382775
winbigcoms <bigcoms6290@gmail.com>
2020-08-06 18:47:02 +0900

    fix scroll

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -4,11 +4,13 @@ import styles from "../styles/Home.module.scss";
 import {LinkOutlined} from "@ant-design/icons";
 
 export function HomeComponent({loading,error,books,getBooks}) {
-  const [state,setState] = useState({viewBooks:[],sort:""});
+  const [state,setState] = useState({viewBooks:[],sort:"",selectedBook:[{title:"",message:"",author:"",createdAt:""}]});
   const sortWhat = useRef(null);
+  const detailTitle = useRef(null);
   const searchInput = useRef("");
 
   useEffect(()=>{
+    console.log("1")
     getBooks();
   },[getBooks])
 
@@ -35,6 +37,12 @@ export function HomeComponent({loading,error,books,getBooks}) {
     setState(states => ({...states,viewBooks:searchBook.length===0?state.viewBooks:searchBook}));
   }
 
+  function showBookDetail(e){
+    let copyBook = books.filter(book => book.bookId === +e.currentTarget.id);
+    setState(states => ({...states,selectedBook:copyBook}));
+  }
+
+
   return (
     <div className={styles.wrapper}>
       <header className={styles.header}>
@@ -46,46 +54,55 @@ export function HomeComponent({loading,error,books,getBooks}) {
             <option value="createdAt">추가날짜로 정렬</option>
           </select>
         </label>
+        <input type="text"onChange={search} ref={searchInput} className={styles.search} placeholder="검색 할 책을 입력하세요"/>
       </header>
       <div className={styles.main}>
-        <ul className={styles.books}>
-          <h2 className={styles.a11yHidden}>도서</h2>
-          {state.viewBooks.map(book =>(<li key={book.bookId} className={styles.bookBox}>
-              <div className={styles.book}>
-                <ul className={styles.hardcover_front}>
-                  <li style={{
-                    backgroundImage:`url(./images/${book.bookId}.jpg)`,
-                    backgroundRepeat:"no-repeat"
-                  }}>
-                  </li>
-                  <li></li>
-                </ul>
-                <ul className={styles.page}>
-                  <li>
-                    <span className={styles.bookMessage}>
-                      {book.message.length < 30 ? 
-                        book.message :
-                        book.message.slice(0,28)+"..."
-                        }
-                    </span>
-                  </li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                </ul>
+        <div className={styles.bookContainer}>
+          <ul className={styles.books}>
+            <h2 className={styles.a11yHidden}>도서</h2>
+            {state.viewBooks.map(book =>(<li key={book.bookId} className={styles.bookBox} id={book.bookId} onClick={showBookDetail}>
+                <div className={styles.book}>
+                  <ul className={styles.hardcover_front}>
+                    <li style={{
+                      backgroundImage:`url(./images/${book.bookId}.jpg)`,
+                      backgroundRepeat:"no-repeat"
+                    }}>
+                    </li>
+                    <li></li>
+                  </ul>
+                  <ul className={styles.page}>
+                    <li>
+                      <span className={styles.bookMessage}>
+                        {book.message.length < 30 ? 
+                          book.message :
+                          book.message.slice(0,28)+"..."
+                          }
+                      </span>
+                    </li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                  </ul>
+                </div>
+              <div className={styles.divider}></div>
+              <div className={styles.bookInfo}>
+                <h3>제목: {book.title}</h3>
+                <p>저자: {book.author}</p>
+                <p>책장에 추가한 날짜: {book.createdAt.slice(0,16).split("T").join("/")}</p>
+                <p>마지막 업데이트: {book.updatedAt.slice(0,16).split("T").join("/")}</p>
+                <a className={styles.btn} target="_blank" rel="noopener noreferrer" href={book.url} aria-label="yes24로 이동">책보러가기<LinkOutlined /></a>
               </div>
-            <div className={styles.divider}></div>
-            <div className={styles.bookInfo}>
-              <h3>제목: {book.title}</h3>
-              <p>저자: {book.author}</p>
-              <p>책장에 추가한 날짜: {book.createdAt.slice(0,16).split("T").join("/")}</p>
-              <p>마지막 업데이트: {book.updatedAt.slice(0,16).split("T").join("/")}</p>
-              <a className={styles.btn} target="_blank" rel="noopener noreferrer" href={book.url} aria-label="yes24로 이동">책보러가기<LinkOutlined /></a>
-            </div>
-          </li>))}
-        </ul>
-        <div className={styles.functionUI}>
-          <input type="text"onChange={search} ref={searchInput} className={styles.search}/>
+            </li>))}
+          </ul>
+        </div>
+        <div className={styles.functionUI} ref={detailTitle}>
+          {state.selectedBook[0].title !=="" && state.selectedBook.map(book => (<>
+            <h3>제목 : {book.title}</h3>
+            <p>저자 : {book.author}</p>
+            <p>코멘트 : {book.message}</p>
+            <p>추가 날짜 : {book.createdAt}</p>
+          </>
+          ))}
         </div>
       </div>
     </div>
